6c306b17781bce59cbd820d5c6be16c2257fc873
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 19:45:23 +0600

    Create 451A. Game With Sticks.cpp

---
+++ Codeforces/451A. Game With Sticks.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int main()
+ {
+  	  int n,m;
+  	  cin>>n>>m;
+   	 int mn = min(n,m);
+  	  if(mn%2)printf("Akshat");
+  	  else printf("Malvika");
+		return 0;
+ }
