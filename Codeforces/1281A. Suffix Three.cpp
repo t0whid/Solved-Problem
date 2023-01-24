185525ff774e156b97a4782422ae3bc02ed9132d
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 16:20:08 +0600

    Create 1281A. Suffix Three.cpp

---
+++ Codeforces/1281A. Suffix Three.cpp
@@ -0,0 +1,48 @@
+/*  Allah, increase me in knowledge.
+            Towhid Hasan    	        */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast_io  					ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll							long long int
+#define pi							acos(-1.0)
+#define pii							pair<ll,ll>
+#define mp							make_pair
+#define cln(x)						memset(x, 0, sizeof(x))
+#define cln_set(x)					memset(x, -1, sizeof(x))
+#define upper(str)					transform(str.begin(), str.end(), str.begin(), ::toupper)
+#define lower(str)					transform(str.begin(), str.end(), str.begin(), ::tolower)
+bool check = true;
+ll ans = 0, sub = 0, cur = 0, pos = 0, cnt = 0, sum = 0, mul = 0;
+
+void solve()
+{
+    ll n, i, j, x;
+    string s;
+    cin>>s;
+    x = s.size();
+    if(s[x-1] == 'o') {
+		cout<<"FILIPINO"<<endl;
+		return;
+    }
+    if(s[x-1] == 'u') {
+		cout<<"JAPANESE"<<endl;
+		return;
+    }
+    if(s[x-1] == 'a') {
+		cout<<"KOREAN"<<endl;
+		return;
+    }
+}
+
+signed main()
+{
+    fast_io;
+    int tc;
+    cin>>tc;
+    while(tc--)
+        solve();
+    return 0;
+}
+
+
