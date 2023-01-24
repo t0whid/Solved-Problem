25a2f2d4f0ff2c922a87e556c9695ea4d7e895ce
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 19:29:50 +0600

    Create 678A. Johny Likes Numbers.cpp

---
+++ Codeforces/678A. Johny Likes Numbers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+#define sf(a)                scanf("%lld",&a)
+#define sff(a,b)             scanf("%lld %lld",&a,&b)
+#define case(a,b)            printf("Case %lld: %lld\n",a,b)
+#define pf(a)                printf("%lld\n",a)
+#define pff(a,b)             printf("%lld %lld\n",a,b)
+using namespace std;
+int main()
+{
+    ll n, k, num;
+
+	while(sff(n,k)==2)
+	{
+		num= k*((n/k)+1);
+		pf(num);
+	}
+    return 0;
+}
+
+
+
+ 
