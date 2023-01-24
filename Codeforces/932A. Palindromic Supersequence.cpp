82e758136421a89f8d1da9cefab8677304c2433b
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 19:44:27 +0600

    Create 932A. Palindromic Supersequence.cpp

---
+++ Codeforces/932A. Palindromic Supersequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+#define sf(a)                scanf("%lld",&a)
+#define sff(a,b)             scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)          scanf("%lld %lld %lld",&a,&b,&c)
+#define case(a,b)            printf("Case %lld: %lld\n",a,b)
+#define pf(a)                printf("%lld\n",a)
+#define pff(a,b)             printf("%lld %lld\n",a,b)
+#define pb		             push_back
+#define cln(x)               memset(x, 0, sizeof(x))
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<s;
+    reverse(s.begin(),s.end());
+    cout<<s;
+    return 0;
+}
+
+
+
+
+
+
+
+ 
