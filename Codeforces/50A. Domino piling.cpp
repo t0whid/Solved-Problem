c6d50c2ea62bc2df23f8c8aa5a9e552fd1f8d1ee
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 19:01:36 +0600

    Create 50A. Domino piling.cpp

---
+++ Codeforces/50A. Domino piling.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+#define N 1000000
+int primes[N];
+
+using namespace std;
+
+int main()
+{
+    ll n, m;
+    while(cin>>n>>m)
+    {
+        cout<<(n*m)/2<<endl;
+    }
+    return 0;
+}
+ 
