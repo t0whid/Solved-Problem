ec527ea653b0a0320a4f65f6b0b9ad50986da5d9
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 17:10:25 +0600

    Create 630A. Again Twenty Five!.cpp

---
+++ Codeforces/630A. Again Twenty Five!.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+
+using namespace std;
+
+int main()
+{
+    ll n;
+    while(cin>>n)
+    {
+        cout<<25<<endl;
+    }
+    return 0;
+}
