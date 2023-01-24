8831dfa7675fb88f4ec79d172e4bc5fde3cac0df
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 16:20:46 +0600

    Create 1747A. Two Groups.cpp

---
+++ Codeforces/1747A. Two Groups.cpp
@@ -0,0 +1,84 @@
+/*  Allah, increase me in knowledge.
+            Towhid Hasan    	        */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast_io  					ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll							long long int
+#define pi							acos(-1.0)
+#define pii							pair<ll,ll>
+#define mp							make_pair
+#define cln(x)						memset(x, 0, sizeof(x))
+#define cln_set(x)					memset(x, -1, sizeof(x))
+#define upper(str)					transform(str.begin(), str.end(), str.begin(), ::toupper)
+#define lower(str)					transform(str.begin(), str.end(), str.begin(), ::tolower)
+bool check = true;
+ll ans = 0, sub = 0, cur = 0, pos = 0, cnt = 0, sum = 0, mul = 0;
+
+void solve()
+{
+    ll n, i, j, x = 0;
+    cin>>n;
+    ll arr[n+1];
+    for(i=0; i<n; i++)
+	{
+		ll v;
+		cin>>v;
+		x += v;
+	}
+	cout<<abs(x)<<endl;
+}
+
+signed main()
+{
+    fast_io;
+   int tc;
+    cin>>tc;
+    while(tc--)
+        solve();
+    return 0;
+}
+
+
+
+/*
+sort(v.begin(), v.end());
+lower_bound (v.begin(), v.end(), x)
+upper_bound (v.begin(), v.end(), x)
+lower_bound(v.begin(), v.end(), x) - v.begin()
+upper_bound(v.begin(), v.end(), x) - v.begin()
+vector<int> v(arr, arr + n); -> push array in a vector
+ll nodes, edges, costs;
+vector<ll>edges[20009];
+vector<ll>costs[N];
+vector<ll> ::iterator it;
+bool vis[N] = {};
+bool node_type[N] = {};
+ll dis[N];
+vector<ll> adj[N];
+vector<ll>adj[N];
+s.erase (i,1);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
