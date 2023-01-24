cbe7fa6c1c58ddda6436097385322cb9bb19c0e0
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 17:12:38 +0600

    Create 4A. Watermelon.cpp

---
+++ Codeforces/4A. Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+
+using namespace std;
+
+int main()
+{
+    ll n;
+    while(cin>>n)
+    {
+        if(n&1 || n==2) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
+ 
