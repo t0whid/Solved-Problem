35a34e92ac956db1074897904baee80f3dadafa3
Towhid Hasan <81054358+t0whid@users.noreply.github.com>
2023-01-24 19:59:36 +0600

    Create 733A. Grasshopper And the String.cpp

---
+++ Codeforces/733A. Grasshopper And the String.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi acos(-1.0)
+#define sf(a)                scanf("%lld",&a)
+#define sff(a,b)             scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)          scanf("%lld %lld %lld",&a,&b,&c)
+#define case(a,b)            printf("Case %lld: %lld\n",a,b)
+#define pf(a)                printf("%lld\n",a)
+#define pff(a,b)             printf("%lld %lld\n",a,b)
+#define pb                   push_back
+#define cln(x)               memset(x, 0, sizeof(x))
+const int N=123456;
+int arr[N], i, j;
+using namespace std;
+int main()
+{
+    string s;
+    while(cin>>s)
+    {
+        ll x=s.size(), mx=0, cnt=1;
+        string s1="AEIOUY";
+        for(i=0; i<x; i++)
+        {
+            if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='Y')
+            {
+                cnt=1;
+            }
+            else
+            {
+                cnt++;
+            }
+            mx=max(mx,cnt);
+        }
+        pf(mx);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+ 
